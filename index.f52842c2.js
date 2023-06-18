17ae14d34008e0ca607ad576bc417ae402f1fc1d
florinfire80 <127885092+florinfire80@users.noreply.github.com>
2023-06-18 06:37:15 +0000

    Deploying to gh-pages from @ RalucaElenaB/project-team-IceCream@54a034ff9c1b28ed801263fd7f9fc9e06f4ded7c 🚀

---
+++ index.f52842c2.js
@@ -0,0 +1,2 @@
+new Swiper("#swiper-1",{loop:!0,autoplay:{delay:4e3,disableOnInteraction:!1},pagination:{el:"#swiper-1 .swiper-pagination",clickable:!0}});
+//# sourceMappingURL=index.f52842c2.js.map
